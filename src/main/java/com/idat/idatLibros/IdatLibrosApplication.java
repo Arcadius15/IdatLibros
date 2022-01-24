531bce97e9046b9a7359d78d798a707e1f0ba8cf
Diego Vicente <diegovic99@hotmail.com>
2022-01-23 22:34:34 -0500

    Init

---
+++ src/main/java/com/idat/idatLibros/IdatLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.idat.idatLibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdatLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdatLibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/idatLibros/IdatLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.idatLibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdatLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
